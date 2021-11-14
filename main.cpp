389b9558cb01c37cc3d4818f3733a01cc94e94ef
IgorSerkov <93199459+IgorSerkov@users.noreply.github.com>
2021-11-14 13:12:54 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,246 +1,31 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
-class Date {
-protected:
-	int day, month, year;
-	int days[13] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 };
-public:
-	Date() {
-		day = 0;
-		month = 0;
-		year = 0;
-	}
-	Date(int _day, int _month, int _year) {
-		day = _day;
-		month = _month;
-		year = _year;
-	}
-
-	~Date() {};
-
-
-	friend ostream& operator<<(ostream& out,Date& ob);
-	Date operator+(Date& ob);
-	Date operator-(Date& ob);
-	Date operator++(int _day);
-	Date operator--(int _day);
-	Date operator+(int day);
-	Date operator-(int day);
-
-};
-
-Date Date::operator--(int _day) {
-	Date temp;
-	temp.day = day-1;
-	temp.month = month;
-	temp.year = year;
-
-		if (temp.month == 2 or temp.month == 4 or temp.month == 1 or temp.month == 6 or temp.month == 11 or temp.month == 8 or temp.month == 9) {
-			if (temp.day < 1) {
-				temp.day += 31;
-				temp.month--;
-			}
-		}
-		else if (temp.month == 3) {
-			if (temp.day < 1) {
-				temp.day += 28;
-				temp.month--;
-			}
-		}
-		else {
-			if (temp.day < 1) {
-				temp.day += 30;
-				temp.month;
-			}
-		}
-
-	while (temp.month < 1) {
-		temp.year--;
-		temp.month + 12;
-	}
-	return temp;
-}
-
-Date Date::operator++(int _day) {
-	Date temp;
-	temp.day = day+1;
-	temp.month = month;
-	temp.year = year;
-		if (temp.month == 1 or temp.month == 3 or temp.month == 12 or temp.month == 5 or temp.month == 10 or temp.month == 7 or temp.month == 8) {
-			if (temp.day > 31) {
-				temp.day = 1;
-				temp.month++;
-			}
-		}
-		else if (temp.month == 2) {
-			if (temp.day > 28) {
-				temp.day = 1;
-				temp.month++;
-			}
-		}
-		else {
-			if (temp.day > 30) {
-				temp.day = 1;
-				temp.month++;
-			}
-		}
-
-	while (temp.month > 12) {
-		temp.month - 12;
-		temp.year++;
-	}
-	return temp;
-}
-
-Date Date::operator+(int _day) {
-	Date temp;
-	temp.day = day + _day;
-	temp.month = month;
-	temp.year = year;
-		if (temp.month == 1 or temp.month == 3 or temp.month == 12 or temp.month == 5 or temp.month == 10 or temp.month == 7 or temp.month == 8) {
-			if (temp.day > 31) {
-				temp.day -= 31;
-				temp.month++;
-			}
-		}
-		else if (temp.month == 2) {
-			if (temp.day > 28) {
-				temp.day -= 28;
-				temp.month++;
-			}
-		}
-		else {
-			if (temp.day > 30) {
-				temp.day -= 30;
-				temp.month++;
-			}
-		}
-	while (temp.month > 12) {
-		temp.month -= 12;
-		temp.year++;
-	}
-	return temp;
-}
-
-Date Date::operator-(int _day) {
-	Date temp;
-	temp.day = day - _day;
-	temp.month = month;
-	temp.year = year;
-
-		if (temp.month == 2 or temp.month == 4 or temp.month == 1 or temp.month == 6 or temp.month == 11 or temp.month == 8 or temp.month == 9) {
-			if (temp.day < 1) {
-				temp.day += 31;
-				temp.month--;
-			}
-		}
-		else if (temp.month == 3) {
-			if (temp.day < 1) {
-				temp.day += 28;
-				temp.month--;
-			}
-		}
-		else {
-			if (temp.day < 1) {
-				temp.day += 30;
-				temp.month--;
-			}
-		}
-
-	while (temp.month < 1) {
-		temp.year--;
-		temp.month += 12;
-	}
-	return temp;
-}
-
-Date Date::operator+(Date& ob) {
-	Date temp;
-
-	temp.month = month + ob.month;
-	temp.day = day + ob.day;
-		if (temp.month == 1 or temp.month == 3 or temp.month == 12 or temp.month == 5 or temp.month == 10 or temp.month == 7 or temp.month == 8) {
-			if (temp.day > 31) {
-				temp.day -= 31;
-				temp.month++;
-			}
-		}
-		else if (temp.month == 2) {
-			if (temp.day > 28) {
-				temp.day -= 28;
-				temp.month++;
-			}
-		}
-		else {
-			if (temp.day > 30) {
-				temp.day -= 30;
-				temp.month++;
-			}
-		}
-	while (temp.month > 12) {
-		temp.month -= 12;
-		temp.year++;
-	}
-	temp.year += year + ob.year;
-	return temp;
-}
-
-Date Date::operator-(Date& ob) {
-	Date temp;
-	temp.month = month - ob.month;
-	temp.day = day - ob.day;
-	while (temp.day > 31 or temp.day < 1) {
-		if (temp.month == 2 or temp.month == 4 or temp.month == 1 or temp.month == 6 or temp.month == 11 or temp.month == 8 or temp.month == 9) {
-			if (temp.day < 1) {
-				temp.day += 31;
-				temp.month--;
-			}
-		}
-		else if (temp.month == 3) {
-			if (temp.day > 28) {
-				temp.day += 28;
-				temp.month--;
-			}
-		}
-		else {
-			if (temp.day < 1) {
-				temp.day += 30;
-				temp.month--;
-			}
-		}
-	}
-	while (temp.month < 1) {
-		temp.year--;
-		temp.month += 12;
-	}
-	temp.year = year - ob.year;
-	return temp;
-}
-
-ostream& operator<<(ostream& out, Date& ob){
-
-	return out << ob.day << "." << ob.month<<"."<< ob.year;
-}
+double pointDiff(double x1, double y1, double x2, double y2);
+double pointDiff(double x1, double y1, double z1, double x2, double y2, double z2);
 
 int main() {
-	Date	start(1, 2, 2010), constructing(29, 0, 0);
-	Date end = start + constructing;
-
-
-	cout << "data nachala stroitelstva: " << start << endl;
-	cout << "Vrema stroitelstva: " << constructing << endl;
-	cout << "data sdachi ob'ekta(data+data): " << end << endl;
-	end = start - constructing;
-	cout << "Data nachala stroitelstva ob'ekta(Data-Data): " << end << endl;
-	end = start + 29;
-	cout << "Data sdachi ob'ekta(Data+Int): " << end << endl;
-	end = end - 29;
-	cout << "Data nachala stroitelstva ob'ekta(data-Int): " << end << endl;
-	end = start++;
-	cout << "Data sdachi ob'ekta(Data++): " << end << endl;
-	end = end--;
-	cout << "Data nachala stroitelstva ob'ekta(data--): " << end << endl;
+	cout << "1)dvymernoe prostranstvo: " << endl;
+	double a = pointDiff(-1.0,4.0,-1.0, 2.0);
+	double b = pointDiff(-7.0,3.0,-1.0, 2.0);
+	double c = pointDiff(-1.0,4.0,-7.0, 3.0);
+	double P = a + b + c;
+	cout << "Perimetr treygolnika raven: " << P << endl << endl;
+	cout << "2)trexmernoe prostranstvo: " << endl;
+	a = pointDiff(2.0, 1.0, 3.0, 2.0, 0.0,5.0);
+	b = pointDiff(2.0, 1.0, 3.0, 5.0, -1.0, 10.0);
+	c = pointDiff(5.0, -1.0, 10.0, 2.0, 0.0, 5.0);
+	P = a + b + c;
+	cout << "Perimetr treygolnika raven: " << P << endl << endl;
 	return 0;
+}
+
+double pointDiff(double x1, double y1, double x2, double y2) {
+	return sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
+}
+
+double pointDiff(double x1, double y1, double z1, double x2, double y2, double z2) {
+	return sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2) + pow((z2 - z1), 2));
 }
